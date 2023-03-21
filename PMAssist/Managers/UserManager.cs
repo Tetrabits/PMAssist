704078fba5d11217be7d644ae5e68df1723619a7
Hariprasad Natarajan <hariprasadkvn@hotmail.com>
2023-03-21 13:36:58 +0530

    Optimzed the data pull from calendar service

--- PMAssist/Managers/UserManager.cs
+++ PMAssist/Managers/UserManager.cs
@@ -1,5 +1,6 @@
 ﻿using PMAssist.Interfaces;
 using PMAssist.Models;
+using System;
 using System.Text.Json;
 
 namespace PMAssist.Managers
@@ -8,6 +9,8 @@ namespace PMAssist.Managers
     {
         private static IDataAccessRepository dataAccess;
         private static readonly string URL = @"users";
+        private static IList<UserModel> Users;
+
         internal UserManager(IDataAccessRepository dataAccessRepository)
         {
             dataAccess = dataAccessRepository;
@@ -21,23 +24,51 @@ namespace PMAssist.Managers
         static UserManager()
         {
             dataAccess = new DataAccessRepository();
+            Users = new List<UserModel>();
+
+            var userData = dataAccess.GetAll("asdfsdf", URL).GetAwaiter().GetResult();
+            var rawData = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string,string>>>(userData);
+
+            foreach(var data in rawData) 
+            {
+                Users.Add(new UserModel
+                {
+                    UID = data.Key,
+                    Name = data.Value["name"],
+                    Status = data.Value["active"]=="true"
+                });
+                
+            }
+
         }
 
         public static async Task<UserModel> GetUser(string userId)
         {
-
-            var url = $"{URL}/{userId}";
-
-            var userData = await dataAccess.GetAll("asdfsdf", url);
-
-            var rawData = JsonSerializer.Deserialize<Dictionary<string, string>>(userData);
-
-            if (rawData == null)
+            return await Task.Run(() =>
             {
-                return new UserModel { Name = "Not Defined" };
-            }
+                var user = Users.FirstOrDefault(n => n.UID == userId);
+                if ( user is not null)
+                {
+                    return user;
+                }
+                else
+                {
+                    return new UserModel { Name = "Undefined" };
+                }
+            });
 
-            return new UserModel { Name = rawData["name"] };
+            //var url = $"{URL}/{userId}";
+
+            //var userData = await dataAccess.GetAll("asdfsdf", url);
+
+            //var rawData = JsonSerializer.Deserialize<Dictionary<string, string>>(userData);
+
+            //if (rawData == null)
+            //{
+            //    return new UserModel { Name = "Not Defined" };
+            //}
+
+            //return new UserModel { Name = rawData["name"] };
         }
     }
 }
--- PMAssist/Models/UserModel.cs
+++ PMAssist/Models/UserModel.cs
@@ -2,6 +2,8 @@
 {
     public class UserModel
     {
-        public string Name { get; internal set; }
+        public string Name { get; set; } = string.Empty;
+        public bool Status { get; set; }=false;
+        public string UID { get; internal set; }
     }
 }
