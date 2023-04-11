862b6bb987a465574fd98f87f8305fbac6cdf03b
Hariprasad Natarajan <hariprasadkvn@hotmail.com>
2023-04-11 14:11:16 +0530

    Add API for Project Control

--- PMAssist/Models/Sprint.cs
+++ PMAssist/Models/Sprint.cs
@@ -1,14 +1,23 @@
-﻿namespace PMAssist.Models
+﻿using System.Text.Json.Serialization;
+
+namespace PMAssist.Models
 {
     public class Sprint
     {
+        [JsonPropertyName("sprintnumber")]        
         public int SprintNumber { get; set; } = 0;
+        [JsonPropertyName("startson")]
         public DateTime StartsOn { get; set; }
+        [JsonPropertyName("endson")]
         public DateTime EndsOn { get; set; }
+        [JsonPropertyName("duration")]
         public short Duration { get; set; } = 14;
 
+        [JsonPropertyName("stories")]
         public IEnumerable<Story> Stories { get; set; }
+        [JsonPropertyName("bugs")]
         public IEnumerable<Bug> Bugs { get; set; }
+        [JsonPropertyName("users")]
         public IEnumerable<User> Users { get; set; }
 
     }
