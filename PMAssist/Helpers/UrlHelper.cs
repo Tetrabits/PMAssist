a66ac4bb6b737597c83219b0ab190ee4fd99c679
GajananT <gajanant@emids.com>
2023-04-12 17:42:22 +0530

    Implemented logic to add activity

--- PMAssist/Helpers/UrlHelper.cs
+++ PMAssist/Helpers/UrlHelper.cs
@@ -7,30 +7,30 @@
             private static string baseUrl = "utilize";
             public static string YearUrl(DateTime date)
             {
-               return $"{baseUrl}/{date.Year}";
+                return $"{baseUrl}/{date.Year}";
             }
 
             public static string MonthUrl(DateTime date)
             {
-                return $"{YearUrl(date)}/{date.Month.ToString().PadLeft(2,'0')}";
-                
+                return $"{YearUrl(date)}/{date.Month.ToString().PadLeft(2, '0')}";
+
             }
-            public static string IdUrl(DateTime date,string userId)
+            public static string IdUrl(DateTime date, string userId)
             {
                 return $"{MonthUrl(date)}/{userId}";
             }
 
-            public static string DayUrl(DateTime date,string userId)
+            public static string DayUrl(DateTime date, string userId)
             {
-                return $"{IdUrl(date,userId)}/{date.Day.ToString().PadLeft(2, '0')}";
+                return $"{IdUrl(date, userId)}/{date.Day.ToString().PadLeft(2, '0')}";
             }
 
-            public static string PtoUrl(DateTime date,string userId)
+            public static string PtoUrl(DateTime date, string userId)
             {
-                return $"{DayUrl(date,userId)}/PTO";
+                return $"{DayUrl(date, userId)}/PTO";
             }
         }
-        
+
         public static class Project
         {
             private static string baseUrl = "projects";
@@ -42,7 +42,7 @@
             {
                 return $"{baseUrl}/{projectKey}.json";
             }
-            
+
         }
 
         public static class Sprint
@@ -51,6 +51,38 @@
             public static string SprintUrl(string projectKey)
             {
                 return $"{baseUrl}/{projectKey}.json";
+            }
+            public static string SprintActivityUrl(string projectKey, string userKey)
+            {
+                return $"{baseUrl}/{projectKey}/activities/{userKey}";
+            }
+            public static string SprintBugUrl(string projectKey, string userKey)
+            {
+                return $"{baseUrl}/{projectKey}/bugs/{userKey}";
+            }
+        }
+
+        public static class Story
+        {
+            private static string baseUrl = "stories";
+            public static string StoryUrl(string storyKey)
+            {
+                return $"{baseUrl}/{storyKey}";
+            }
+
+            public static string UserUrl(string storyKey, string userKey)
+            {
+                return $"{baseUrl}/{storyKey}/{userKey}";
+            }
+
+            public static string ActivityUrl(string storyKey, string userKey, string activityKey)
+            {
+                return $"{baseUrl}/{storyKey}/{userKey}/{activityKey}";
+            }
+
+            public static string ActivityFieldUrl(string storyKey, string userKey, string activityKey, string activityFiledKey)
+            {
+                return $"{baseUrl}/{storyKey}/{userKey}/{activityKey}/{activityFiledKey}";
             }
         }
     }
