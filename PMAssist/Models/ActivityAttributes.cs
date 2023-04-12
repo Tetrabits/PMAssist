60d776867f34aba14db070138332f4655d3a03df
GajananT <gajanant@emids.com>
2023-04-12 17:42:22 +0530

    Implemented logic to add activity

---
+++ PMAssist/Models/ActivityAttributes.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PMAssist.Models
+{
+    public class ActivityAttributes
+    {
+        [JsonPropertyName("client")] public bool Client { get; set; }
+        [JsonPropertyName("closedOn")] public DateTime ClosedOn { get; set; }
+        [JsonPropertyName("createdOn")] public DateTime CreatedOn { get; set; }
+        [JsonPropertyName("id")] public string Id { get; set; } = string.Empty;
+        [JsonPropertyName("plan")] public string Plan { get; set; } = string.Empty;
+        [JsonPropertyName("status")] public string Status { get; set; } = string.Empty;
+        [JsonPropertyName("type")] public string Type { get; set; } = string.Empty;
+        [JsonPropertyName("what")] public string What { get; set; } = string.Empty;
+    }
+}
