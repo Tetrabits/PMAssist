2b572c31a2e55137c9666740daec1d3f3d2d7837
Hariprasad Natarajan <hariprasadkvn@hotmail.com>
2023-04-17 14:37:42 +0530

    temp checkin

---
+++ PMAssist/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PMAssist.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("");
+
+        }
+    }
+}
