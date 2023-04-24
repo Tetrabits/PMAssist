d09351e3bd966278d140a2a5eb9af4e689f9c8ee
Hariprasad Natarajan <hariprasadkvn@hotmail.com>
2023-04-24 12:00:35 +0530

    solved conflict

--- PMAssist/Models/Story.cs
+++ PMAssist/Models/Story.cs
@@ -7,5 +7,6 @@
         public string Status { get; set; } = string.Empty;
         public short StoryPoint { get; set; } = 0;
         public Dictionary<string, Dictionary<string, Activity>> Users { get; set; } = new Dictionary<string, Dictionary<string, Activity>>();
+        public DateTime Created { get; set; }
     }
 }
